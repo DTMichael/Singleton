51b3f598b52ff714e801453b088944f99b9f4449
86157 <86157@192.168.125.1>
2019-11-28 23:08:01 +0800

    懒汉式

--- Singleton/src/type2/SingLeton03.java
+++ Singleton/src/type2/SingLeton03.java
@@ -11,5 +11,11 @@ public class SingLeton03 {
 		}
 		return instance;
 	}
+	
+	public static void main(String[] args) {
+		SingLeton03 singLeton03=SingLeton03.getInstance();
+		SingLeton03 singLeton032=SingLeton03.getInstance();
+		System.out.println(singLeton03==singLeton032);
+	}
 
 }
